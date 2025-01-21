c5db1b2e1f6ecb0c92ba730db3cf761a9034d46f
Kajal Pandit <kajalpandit830@gmail.com>
2025-01-21 14:38:37 +0530

    Initial commit

---
+++ todo.js
@@ -0,0 +1,46 @@
+let addTaskBtn = document.querySelector(".addTask");
+let rmvAllTask = document.querySelector(".rmvAllTask");
+let itemNo = 0;
+addTaskBtn.addEventListener("click", () => {
+    let task = document.querySelector(".task");
+    if(task.value != "")
+        addTodo(task.value, itemNo);
+    task.value = "";  //refresh input value
+    itemNo++;
+});
+
+let ul = document.querySelector(".showTodos ul");
+//add the task
+function addTodo(task, itemNo) {
+    let li = document.createElement("li");
+    li.innerHTML = `<input type="checkbox" id="${itemNo}" name="task"><label for="task">${task}</label> &nbsp; <button class="rmvTask btns">X</button>`;
+    ul.appendChild(li);
+    if(ul.childElementCount >= 0)
+        rmvAllTask.style.opacity = "1";
+    
+}
+
+ul.addEventListener("click", (event) => {
+    //remove the task
+   
+    if(event.target.classList.contains("rmvTask")){
+        let itsLi = event.target.parentElement;
+        itsLi.remove();
+    }
+
+    if(ul.childElementCount == 0)
+        rmvAllTask.style.opacity = "0.5";
+
+    //check and cut the task
+    if(event.target.checked == true){
+       let itsLabel = event.target.nextElementSibling;
+       itsLabel.style.textDecoration = "line-through";
+    }
+});
+//remove all task
+rmvAllTask.addEventListener("click", () => {
+    ul.innerHTML = "";
+    rmvAllTask.style.opacity = "0.5";
+});
+
+
